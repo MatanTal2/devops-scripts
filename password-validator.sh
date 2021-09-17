cf4104052ce0d4e2905d41c2a47e762173ad70e0
Matan Tal <matantal2@gmail.com>
2021-09-17 14:29:08 +0300

    add invalid password respond with logic, and a global variable for number of chars in password

--- password-validator.sh
+++ password-validator.sh
@@ -5,6 +5,7 @@ USAGE="Usage: ./password_validator <your_password>"
 red_color=`tput setaf 1`
 green_color=`tput setaf 2`
 reset_color=`tput sgr0`
+NUMBER_OF_DIGITS=10
 
 # The validator function get the password  and validate:
 # minimum 10 chars, lowercase and uppercase letters.
@@ -20,6 +21,22 @@ function password_validator(){
 function valid_password_respond(){
 	echo "${green_color}valid password${reset_color}"
 	exit 0
+}
+
+# invalid respond - I decided to rechek the password for message error
+function invalid_password_respond(){
+	echo "${red_color}Invalid password${reset_color}"
+	# check for number of digits
+	if [[ ${#password} -lt ${NUMBER_OF_DIGITS} ]]; then
+		echo "Must have 10 charactor"
+	elif [[ ${password} != *[[:lower:]]* ]]; then
+		echo "Must have lowercase charactor"
+	elif [[ ${password} != *[[:upper:]]* ]]; then
+		echo "Must have uppercase charactor"
+	elif [[ ${password} != *[0-9]* ]]; then
+		echo "Must have digit"
+	fi
+	exit 1
 }
 
 # Usage function in case the user didn't type he password
