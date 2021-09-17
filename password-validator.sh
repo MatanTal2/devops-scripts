cafc32328fffa1f6131d9d4439acab0a9308b98d
Matan Tal <matantal2@gmail.com>
2021-09-17 14:22:04 +0300

    add function: Usage in case the user didnt type password

--- password-validator.sh
+++ password-validator.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
+# global vairiabels
+USAGE="Usage: ./password_validator <your_password>"
+
+
 # The validator function get the password  and validate:
 # minimum 10 chars, lowercase and uppercase letters.
 function password_validator(){
 	echo "validator function"
 }
 
+# Usage function in case the user didn't type he password
+# prompt text
+function prompt_usage(){
+	echo "$USAGE"
+	exit 1
+}
 
 if [ $# == 1 ]; then
 	password=$1
